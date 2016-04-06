0f4a5aeaf7e3bf40d4f043a99282aabac16b29c2
John Clark <johngclark36@gmail.com>
2016-04-06 07:00:01 -0400

    Add Board#fill_back_row

--- board.rb
+++ board.rb
@@ -1,6 +1,7 @@
 require_relative 'pieces.rb'
 
 class Board
+
   def initialize
     @grid = make_grid
     @sentinel = NullPiece.instance
@@ -12,8 +13,27 @@ class Board
 
   end
 
-  def make_grid
-    Array.new(8) { Array.new(8, "foo")}
+  def fill_back_row(color)
+    pieces = [Rook, Knight, Bishop, Queen, King, Bishop, Rook]
+
+    i = (color == :white) ? 7 : 0
+
+    pieces.each_with_index do |piece_class, j|
+      piece_class.new(color, self, [i,j])
+    end
 
   end
+
+  def make_grid
+    Array.new(8) { Array.new(8, sentinel)}
+    [:white, :black].each do |color|
+      fill_back_row(color)
+      fill_pawn_row(color)
+    end
+
+  end
+
+  private
+
+  attr_reader :sentinel
 end
