e587762d06db162f450a2536f513276088a5e0f7
John Clark <johngclark36@gmail.com>
2016-04-05 21:32:24 -0400

    Add Board#make_grid and #move, store 2d array in @grid

--- board.rb
+++ board.rb
@@ -1,7 +1,19 @@
+require_relative 'pieces.rb'
+
 class Board
+  def initialize
+    @grid = make_grid
+    @sentinel = NullPiece.instance
+
+  end
+
+  def move(start, end_pos)
 
 
+  end
 
+  def make_grid
+    Array.new(8) { Array.new(8, "foo")}
 
-
+  end
 end
