aa9ad3f31842e033420a0c8a95c0c75e18d16b9a
John Clark <johngclark36@gmail.com>
2016-04-06 20:08:51 -0400

    Add SteppingPiece#moves

--- pieces/stepping_pieces.rb
+++ pieces/stepping_pieces.rb
@@ -1,4 +1,16 @@
 require_relative 'piece.rb'
 class SteppingPiece < Piece
-  
+  attr_reader :position
+  def initialize(color, board, position)
+    super(color, board, position)
+  end
+
+  def moves
+    moves = []
+    pos = position
+    move_dirs.each do |dir|
+      moves << [pos[0] + dir[0], pos[1] + dir[1]]
+    end
+    moves
+  end
 end
