431d611c26fdc26ced978e37b76f9b3bc8a9156a
John Clark <johngclark36@gmail.com>
2016-04-06 20:17:33 -0400

    Add Knight Class which inherits from SteppingPiece

--- pieces.rb
+++ pieces.rb
@@ -1,3 +1,6 @@
 require_relative 'pieces/piece'
 require_relative 'pieces/bishop'
 require_relative 'pieces/rook'
+require_relative 'pieces/king'
+require_relative 'pieces/knight'
+require_relative 'pieces/pawn'
---
+++ pieces/knight.rb
@@ -0,0 +1,13 @@
+require_relative 'piece'
+class Knight < SteppingPiece
+  def initialize(color, board, position)
+    super(color, board, position)
+  end
+
+  def move_dirs
+
+  end
+
+
+
+end
